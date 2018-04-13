0a789febbdf06b22e72a2cddec08cd41f04fccc5
Ben Harris <ben@tilde.team>
2018-04-13 00:19:07 -0400

    init

---
+++ index.php
@@ -0,0 +1,20 @@
+<html>
+<head>
+<link rel="stylesheet" href="https://tilde.team/css/hacker.css">
+<title>it's really not</title>
+</head>
+
+<body>
+<div class="container">
+it's really not
+
+
+<p>so it was a week where tree of probabilities was the nightfall</p>
+<p>the dialogue line "it's really not" from the guardian's ghost kept sticking out to us</p>
+<p>looks like this domain was available</p>
+<p>so here we are</p>
+<img src="https://cdn.discordapp.com/attachments/160876561350787072/434202917541511169/tumblr_miz81k8KfB1s5c076o1_500.gif">
+</div>
+</body>
+</html>
+
